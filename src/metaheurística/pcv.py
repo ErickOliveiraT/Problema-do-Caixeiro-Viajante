08575323285ce0a200ba347ca6b5672d155e0574
Erick <erick.teixeira_50@hotmail.com>
2020-07-03 05:24:22 -0300

    2-opt

--- src/metaheurística/pcv.py
+++ src/metaheurística/pcv.py
@@ -2,8 +2,9 @@ from random import randint
 from time import time
 
 #Config:
-instance = open('../../instances/instance_1000.txt', 'r') #Instância (pasta src/instances)
+instance = open('../../instances/instance_500.txt', 'r') #Instância (pasta src/instances)
 prob_mutacao = 0.05 #Probabilidade de mutação do algoritmo genético
+max_deep = 2
 
 def carregarMatriz(): #Lê uma matriz de adjacência
     n = int(instance.readline())
@@ -52,29 +53,56 @@ def swap_random(caminho):
     i2 = randint(1, len(caminho)-1)
     return swap(caminho, i1, i2)
 
-def swap(caminho, i1, i2):
-    temp = caminho[i1]
-    caminho[i1] = caminho[i2]
-    caminho[i2] = temp
-    return caminho
-
 def random_choice():
     rd = randint(1,100)
     if rd <= 5:
         return True
     return False
 
-def otimizar(caminho, m):
+def swap(caminho, i1, i2):
+    temp = caminho[i1]
+    caminho[i1] = caminho[i2]
+    caminho[i2] = temp
+    return caminho
+
+def get_melhor_variacao(caminho, m):
     melhor_caminho = caminho
     melhor_custo = calc_custo(caminho, m)
-    for i in range(1, int(len(caminho)/2)):
-        novo = swap(caminho, i, len(caminho)-1-i)
-        custo = calc_custo(novo, m)
-        if custo < melhor_custo:
-            melhor_custo = custo
-            melhor_caminho = novo
-            print('Um caminho melhor de custo {} foi encontrado!'.format(custo))
+    count = 0
+    for i in range(1, len(caminho)-2):
+        for j in range(i+1, len(caminho)-1):
+            count += 1
+            path = caminho.copy()
+            novo = swap(path, i, j)
+            custo = calc_custo(novo, m)
+            if custo < melhor_custo:
+                melhor_custo = custo
+                melhor_caminho = novo
     return melhor_caminho
+
+def trocar_pares(caminho, m):
+    solutions = []
+    for i in range(1, len(caminho)-2):
+        for j in range(i+1, len(caminho)-1):
+            path = caminho.copy()
+            novo = swap(path, i, j)
+            solutions.append(novo)
+    return solutions
+
+def otimizar(caminho, m):
+    best_caminho = caminho
+    best_custo = calc_custo(best_caminho, m)
+    pares = trocar_pares(caminho, m)
+    testes = pow(len(pares), 2)
+    for par in pares:
+        melhor_par = get_melhor_variacao(par, m)
+        custo = calc_custo(melhor_par, m)
+        if custo < best_custo:
+            print('Um caminho melhor de custo {} foi encontrado!'.format(custo))
+            best_caminho = melhor_par
+            best_custo = custo
+    print('\nOtimização finalizada. {} soluções foram testadas'.format(testes))
+    return best_caminho
 
 print('Carregando matriz\n')
 inicio = time()
@@ -95,11 +123,16 @@ print('Custo inicial: {}\n'.format(calc_custo(C,M)))
 
 print('Otimizando Caminho...\n')
 inicio = time()
+melhor_caminho = C
+melhor_custo = calc_custo(C, M)
 C2 = otimizar(C, M)
 fim = time()
 tempo_otimizacao = round(fim - inicio, 2)
 
 tempo = tempo_carregar + tempo_guloso + tempo_otimizacao
+custo = calc_custo(C2, M)
+print('\nCaminho final: {}'.format(C2))
+print('\nCusto final: {}'.format(custo))
 
 print('\nTempo para carregar a matriz: {} seg'.format(tempo_carregar))
 print('Tempo da resolução gulosa: {} seg'.format(tempo_guloso))
